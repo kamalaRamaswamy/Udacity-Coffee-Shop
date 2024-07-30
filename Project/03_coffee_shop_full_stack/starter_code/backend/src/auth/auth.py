4d659ab252009be50aea8a1fb2534f88b09d0403
Kamala <kamala.ramaswamyk@gmail.com>
2024-07-30 10:46:10 +0530

    Updated auth

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -56,15 +56,12 @@ def get_token_auth_header():
 '''
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
-         raise AuthError({
-            'code': 'invalid_claims',
-            'description': 'Permissions not included in the token!'
-        }, 400)
+        abort(400)
     if permission not in payload['permissions']:
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permissions not found!'
-        }, 403)
+        }, 401)
 
     return True
 
